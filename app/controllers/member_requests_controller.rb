2273c5fe92e1a649cd1ceaf0a1c034f3c7f310a4
Lawrence Chang <lc.uclakers@gmail.com>
2014-06-27 19:56:44 -0400

    mailer testing

--- app/controllers/member_requests_controller.rb
+++ app/controllers/member_requests_controller.rb
@@ -1,14 +1,12 @@
 class MemberRequestsController < ApplicationController
 
   def create
-    # @member_request = MemberRequest.new(member_request_params)
     @member_request = MemberRequest.new
     @member_request.group = Group.find(params[:group_id])
     @member_request.user = current_user
     if @member_request.save
-      # , @member_request.group
-      MemberRequestMailer.new_member_request(current_user).deliver
-      # MemberRequestMailer.request_notification(current_user, @member_request.group).deliver
+      MemberRequestMailer.new_member_request(current_user, @member_request.group).deliver
+      MemberRequestMailer.request_notification(current_user, @member_request.group).deliver
       flash[:notice] = "Successfully sent your request."
       redirect_to groups_path
     else
--- app/mailers/member_request_mailer.rb
+++ app/mailers/member_request_mailer.rb
@@ -1,17 +1,17 @@
 class MemberRequestMailer < ActionMailer::Base
   default from: "sixtwelveinfo@gmail.com"
 
-  def new_member_request(user)
+  def new_member_request(user, group)
     @user = user
-    # @group = group
+    @group = group
 
     mail(to: @user.email, subject: 'Request to join group received.')
   end
 
-  # def request_notification(user, group)
-  #   @user = user
-  #   @group = group
+  def request_notification(user, group)
+    @user = user
+    @group = group
 
-  #   mail(to: 'sixtwelveinfo@gmail.com', subject: 'New member request')
-  # end
+    mail(to: 'sixtwelveinfo@gmail.com', subject: 'New member request')
+  end
 end
